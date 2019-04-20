dc4523aa675b39c03f7dbb6fc3da875a82de2bcf
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-04-20 23:55:57 +0530

    Update GenericFunction.java

--- SeleniumProject/src/main/com/selenium/linkedIn/GenericFunction.java
+++ SeleniumProject/src/main/com/selenium/linkedIn/GenericFunction.java
@@ -1,4 +1,10 @@
 
-public class GenericFunction {
-
+public class GenericFunction
+{
+      public void loadConfig()
+      {
+        File file = new File("Config/config.properties");
+        FileReader reader = new FileReader();
+        
+      }
 }
