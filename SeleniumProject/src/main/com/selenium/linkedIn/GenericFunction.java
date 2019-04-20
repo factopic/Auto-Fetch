f11c400a7ef0ed21b04d9910d209fa7355fc450e
Pavan Kumar <33474677+factopic@users.noreply.github.com>
2019-04-20 23:58:38 +0530

    add loadConfig method

--- SeleniumProject/src/main/com/selenium/linkedIn/GenericFunction.java
+++ SeleniumProject/src/main/com/selenium/linkedIn/GenericFunction.java
@@ -1,10 +1,19 @@
+import java.util.*;
 
 public class GenericFunction
 {
+      Properties props = new Properties();
+      
       public void loadConfig()
       {
         File file = new File("Config/config.properties");
         FileReader reader = new FileReader();
-        
+        props.load(reader);             
       }
+      
+      public void loadChromeDriver()
+      {
+            
+      }
+      
 }
